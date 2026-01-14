0fadabdc2c8d84633b2b23d94aaa52ea7a339eeb
Dmytro Pen <dimapen57@gmail.com>
2026-01-14 21:14:05 +0000

    home page with charts

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,13 +20,14 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en" className="dark" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        suppressHydrationWarning
       >
         {children}
       </body>
